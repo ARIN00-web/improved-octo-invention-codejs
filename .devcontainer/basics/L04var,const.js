bc62670af880f840d7357a055fc322d0b4701c8d
ARIN00-web <kaushikarin23@gmail.com>
2025-03-10 19:09:02 +0000

    datatypes

---
+++ .devcontainer/basics/L04var,const.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "kaushikarin23@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+/* const doesnt change once declared var can be changed*/ 
+ //accoundId = 2
+
+  accountEmail = "kaushik23@gmail.com"
+ accountPassword = "1234"
+accountCity = "udaipur"
+let accountState;
+ console.log(accountId);
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+ //var is not used now a days beacuse of issue in block scope and functional scopeand console.table is used to make tabular data in which square bracket is used
---
+++ .devcontainer/basics/L05_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//for using newer version
+//alert( 3+ 3)//alert is not defined initially in node
+
+console.log(3
+    +3//this is not wrong but code readability should be high ,for the same reason we should not use semicolon too much
+)//use ECMA for reading java documentation
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//number=>2 to power 53
+//bigint 
+//string= "  "
+//boolean = true/false
+//null =>standalone value
+//undefined => means value not assigned
+//symbol => unique 
+
+//object
+
+console.log(typeof age)// to tell typeof like character of integer etc...
+//typeof undefined is undefinedv and typeof null is object
