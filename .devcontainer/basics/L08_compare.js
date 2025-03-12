5ca9a25dea906a97f9544d72e471f5700c46799b
ARIN00-web <kaushikarin23@gmail.com>
2025-03-12 20:53:01 +0000

    datatypes

--- .devcontainer/basics/L08_compare.js
+++ .devcontainer/basics/L08_compare.js
@@ -4,6 +4,22 @@
 //console.log(2 == 1);
 //console.log(2 != 1);
 
-console.log("02" > 1);
-console.log("02" > 1);
+console.log("2" > 1);//doesnt check datatype but possibility of error
+
+console.log("02" > 1);// doesnt checks datatype and possibility of error
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined < 0);
+console.log(undefined == 0);
+console.log(undefined > 0);
+
+// ===
+
+console.log("2" === 2); //strcitly checks datatype also
+
+
 
