8c56c7e8e9702fd9faef42885c2ad42b9d7fbe96
ARIN00-web <kaushikarin23@gmail.com>
2025-03-14 18:57:46 +0000

    datatypes

--- .devcontainer/basics/L11_strings.js
+++ .devcontainer/basics/L11_strings.js
@@ -14,7 +14,7 @@ console.log(gameName.length);
 console.log(gameName.toUpperCase());
 
 console.log(gameName.charAt(2));
-console.log(gameName.indexof('t'));
+console.log(gameName.indexOf('t'));
 //read at MDN docs
 
 const newString = gameName.substring(0,4)
@@ -22,6 +22,19 @@ console.log(newString);   // gives charcter from 0 to 3 place
 
 const anotherString = gameName.slice(-8.4)
 console.log(anotherString);//same but we can have negative sign also
+
+const newStringOne = "    hitesh     "
+console.log(newStringOne);
+console.log(newStringOne.trim());//it trims the blank spaces
+//trim start for removing start spaces and trim end for end spaces
+
+const url = "https://hitesh.com/hitesh%20chaudhary"//space between urls is converted into%20  by the computer
+
+console.log(url.replace('%20','-'));//replaces%20 with -
+console.log(url.includes('hitesh'));//to check if hitesh word is p0resent in url
+console.log(url.split('/'));//splits whenever /comes or anyhting inside the argument if computer sees in url splits that from there
+
+
 
 
 
---
+++ .devcontainer/basics/L12_nummath.js
@@ -0,0 +1,23 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)// defining our data is number
+
+console.log(balance);
+
+console.log(balance.toString().length)//to string checks if it iss string then number of didgit is number of charcaters
+console.log(balance.toFixed(2));//precision value upto  aftre point
+
+const otherNumber = 123.6574
+
+console.log(otherNumber.toPrecision(3));// starting three value after rounding from start and gives in exponential if large value
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'));// to convert commas according to indian syaytem
+
+//**********MATHS*********** */
+
+console.log(math);
+
+
+
+
