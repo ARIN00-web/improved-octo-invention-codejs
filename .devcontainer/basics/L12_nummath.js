e71ace8967e24afd9dca90803581154d5ac89d81
ARIN00-web <kaushikarin23@gmail.com>
2025-03-15 18:56:34 +0000

    dataypes

--- .devcontainer/basics/L12_nummath.js
+++ .devcontainer/basics/L12_nummath.js
@@ -16,7 +16,30 @@ console.log(hundreds.toLocaleString('en-IN'));// to convert commas according to 
 
 //**********MATHS*********** */
 
-console.log(math);
+console.log(Math);
+console.log(Math.abs(-4));//absolute value like only positive value
+console.log(Math.round(4.3));//round off
+console.log(Math.ceil(4.3));//round off to upper integer
+console.log(Math.floor(4.3));//round off to lower integer
+console.log(Math.min(4,3,5,6,7));//to get the min number simlarly math.max
+console.log(Math.random());//random value between 0 to 1
+//now we can multiply by tha range we want to take numbers from
+console.log(Math.random()*10);
+console.log(Math.random()*10+1);//now the range is 1 to 11
+console.log(Math.floor(Math.random()*10)+1);//all values get rounded off
+const min = 10
+const max = 20
+
+
+console.log(Math.random() *  (max - min + 1));
+console.log(Math.floor(Math.random() *  (max - min + 1)) + min);// value between the given range
+
+
+
+
+
+
+
 
 
 
