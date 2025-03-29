82506bb3b341e369e80d9bdc08e0ebbaa8fb755b
ARIN00-web <kaushikarin23@gmail.com>
2025-03-29 21:31:56 +0000

    datatypes

---
+++ .devcontainer/part 2/L18_object3.js
@@ -0,0 +1,31 @@
+//destructuring of objects
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+//course.courseInstructor
+
+//const {courseInstructor} = course
+const {courseInstructor: instructor} = course //renaming couirseinstruictor to instructor
+
+
+//console.log(courseInstructor);
+console.log(instructor);
+
+
+//apna kaam kisi aur ke sar pe daalne ko API khte hain
+
+//{
+  //  "name": "hitesh",
+    //"coursename": "js in hindi",
+    //
+    // "price": "free"
+//}
+
+[
+{},
+{},
+
+]
