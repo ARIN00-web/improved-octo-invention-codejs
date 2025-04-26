d90feaf9e58d60a261e93b86af3cdf4a012f3594
Arin kaushik <kaushikarin23@gmail.com>
2025-04-26 22:50:10 +0530

    datatypes

--- functionsnall/ASyncAwait.js
+++ functionsnall/ASyncAwait.js
@@ -1,7 +1,39 @@
-async function harry(){
-    return 5
-}
+async function arin(){
+let delhiweather = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("27 Deg")
 
-harry().then((x)=>{
-    alert(x)
+    },1000)
 })
+
+
+let Bangaloreweather = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("21 Deg")
+
+    },2000)
+})
+
+// delhiweather.then(alert)
+// Bangaloreweather.then(alert)
+console.log("fetching delhi weather please wait")
+let delhiW = await delhiweather// code wont go further until await promise is solved
+
+console.log("fetching Bangalore weather please wait")
+let BangaloreW = await Bangaloreweather
+console.log("fecthed Bangalore Weather:" + BangaloreW)
+return[delhiW,BangaloreW]
+}
+console.log("welcome to weather conrtol room")
+
+arin()
+
+// async function harry(){
+//     return 5
+// }
+
+// harry().then((x)=>{
+//     alert(x)
+// })
+
+
