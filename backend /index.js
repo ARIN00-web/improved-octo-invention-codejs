88df91464a1362a1d143d46f08a9a65acc65cc0f
Arin kaushik <kaushikarin23@gmail.com>
2025-04-27 00:06:00 +0530

    datatypes

--- backend /index.js
+++ backend /index.js
@@ -1,1 +1,8 @@
-console.log("hello world")
+
+const catme = require('cat-me');
+
+ console.log(catme())
+
+ //npm we use to install npm codes from npm website npm is installed with node js automatically
+
+ 
