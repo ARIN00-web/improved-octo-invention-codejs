f5974502743a5b3db9483f81a23ff5eba685904c
Arin kaushik <kaushikarin23@gmail.com>
2025-04-30 01:57:01 +0530

    expresso

--- backend /expressIt.js
+++ backend /expressIt.js
@@ -4,6 +4,11 @@ const app = express();
 app.get('/arin', (req, res) => {
     res.send("This is Arin bitch");
 })
+app.get('/about',(req,res) => {
+    res.send('about page')})
+
+    app.get('/profile',(req,res) => {
+        res.send('profile page')})
 
 app.listen(3000, '0.0.0.0', () => {
     console.log(`Listening on 3000...`);
--- backend /index.js
+++ backend /index.js
@@ -4,5 +4,3 @@ const catme = require('cat-me');
  console.log(catme())
 
  //npm we use to install npm codes from npm website npm is installed with node js automatically
-
- 
