381c03954287de4c22cea4bdc69bb278adf0fb05
Arin kaushik <kaushikarin23@gmail.com>
2025-05-01 15:45:22 +0530

    express

--- backend /expressIt.js
+++ backend /expressIt.js
@@ -1,14 +1,46 @@
 const express = require('express');
 const app = express();
 
-app.get('/arin', (req, res) => {
-    res.send("This is Arin bitch");
+const morgan = require('morgan')
+app.use(morgan('dev'))
+
+app.use(express.json())
+app.use(express.urlencoded({exended:true}))// to get data in req.body these two are used
+app.set("view engine",'ejs')
+
+// app.get('/', (req, res) => {
+//     res.render('index');
+// })
+app.use((req,res,next) => {
+   
+    const a = 2
+    const b = 3
+
+    console.log(a+b)
+    return next()//after next we can go to other routes
 })
-app.get('/about',(req,res) => {
-    res.send('about page')})
+
+
+app.get('/arin', (req, res) => {
+    console.log(req);
+    res.json({
+        name: "terabaap"
+    })
+})
+
+app.get('/about', (req,res) => {
+    res.render('index')})
+
+    //  app.post('/get-form-data', (req,res) => {
+    //     console.log(req.query)
+    //     res.send('data recieved')})
+
+    app.post('/get-form-data', (req,res) => {
+        console.log(req.body)
+        res.send('data recieved')})
 
     app.get('/profile',(req,res) => {
-        res.send('profile page')})
+        res.send("profile page")})
 
 app.listen(3000, '0.0.0.0', () => {
     console.log(`Listening on 3000...`);
---
+++ insta.md
@@ -0,0 +1,13 @@
+## features
+/signup -> email password ..... -> store mongodb
+/signin -> email password ..... -> db match 
+
+### only signined
+/home -> 10-20 photo with like button (store in db)
+/profile -> photo username email liked photos .....
+/about 
+/contact
+
+## learn 
+- jwt authentication + middleware
+- mongo db
